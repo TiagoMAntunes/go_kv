d342fa27c8646cd4930d91a20a027296fb52407f
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2022-10-10 09:49:49 +0200

    Implement client CLI logic

--- client.go
+++ client.go
@@ -1,7 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func get_entry(ctx *cli.Context) error {
+	key := ctx.String("key")
+
+	fmt.Printf("Get %s\n", key)
+
+	return nil
+}
+
+func list_entries(ctx *cli.Context) error {
+	fmt.Println("List...")
+	return nil
+}
+
+func add_entry(ctx *cli.Context) error {
+	key := ctx.String("key")
+	value := ctx.String("value")
+
+	fmt.Printf("Add %s: %s\n", key, value)
+
+	return nil
+}
+
+func remove_entry(ctx *cli.Context) error {
+	key := ctx.String("key")
+	value := ctx.String("value")
+
+	fmt.Printf("Remove %s: %s\n", key, value)
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello client!")
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "Adds an entry to the Key-Value server",
+				Action:  add_entry,
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "key", Aliases: []string{"k"}, Required: true},
+					&cli.StringFlag{Name: "value", Aliases: []string{"v"}, Required: true},
+				},
+			},
+			{
+				Name:    "remove",
+				Aliases: []string{"r", "rm"},
+				Usage:   "Removes the specified entry from the Key-Value server, if it exists",
+				Action:  remove_entry,
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "key", Aliases: []string{"k"}, Required: true},
+					&cli.StringFlag{Name: "value", Aliases: []string{"v"}, Required: true},
+				},
+			},
+			{
+				Name:    "list",
+				Aliases: []string{"l", "ls"},
+				Usage:   "Prints out all the existing Key-Value pairs",
+				Action:  list_entries,
+			},
+			{
+				Name:    "get",
+				Aliases: []string{"g"},
+				Usage:   "Prints the given Key-Value pair, if it exists",
+				Action:  get_entry,
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "key", Aliases: []string{"k"}, Required: true},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.18
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gofiber/fiber/v2 v2.38.1 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.19.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
